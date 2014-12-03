40ba903cc716843a88deea1b577a5d50a6971fda
ravaliB <benjamin.ravalijaona@gmail.com>
2014-12-03 11:58:52 +0100

    forward_additive projet test added

---
+++ VisualStudio_PFEE_TI/forward_additive/test/test/forwadditive.h
@@ -0,0 +1,8 @@
+#ifndef __FORWADDITIVE_H__
+#define __FORWADDITIVE_H__
+
+#include <opencv2\opencv.hpp>
+
+void align_image_forwards_additive(IplImage* pImgT, CvRect omega, IplImage* pImgI);
+
+#endif //__FORWADDITIVE_H__
---
+++ VisualStudio_PFEE_TI/forward_additive/test/test/invcomp.h
@@ -0,0 +1,8 @@
+#ifndef __INVERSECOMP_H__
+#define __INVERSECOMP_H__
+
+#include <opencv2\opencv.hpp>
+
+void align_image_inverse_compositional(IplImage* pImgT, CvRect omega, IplImage* pImgI);
+
+#endif //__INVERSECOMP_H__
