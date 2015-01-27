5989d94a893be44b316ec9302c2fcb7bcc18a933
ravaliB <benjamin.ravalijaona@gmail.com>
2015-01-27 15:47:32 +0100

    reorganisation VS Traitement Image

--- VisualStudio_PFEE_TI/forward_additive/test/test/forwadditive.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FORWADDITIVE_H__
-#define __FORWADDITIVE_H__
-
-#include <opencv2\opencv.hpp>
-
-void align_image_forwards_additive(IplImage* pImgT, CvRect omega, IplImage* pImgI);
-
-#endif //__FORWADDITIVE_H__
--- VisualStudio_PFEE_TI/forward_additive/test/test/invcomp.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INVERSECOMP_H__
-#define __INVERSECOMP_H__
-
-#include <opencv2\opencv.hpp>
-
-void align_image_inverse_compositional(IplImage* pImgT, CvRect omega, IplImage* pImgI);
-
-#endif //__INVERSECOMP_H__
